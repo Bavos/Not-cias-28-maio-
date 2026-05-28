04e741f43f9b6fb568afaf07af216e2d4374518f
Bavos <87132712+Bavos@users.noreply.github.com>
2026-05-28 08:40:48 -0300

    Update AnimatedBackground.tsx

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -1,4 +1,4 @@
-          import React from "react";
+import React from "react";
 import {AbsoluteFill, interpolate, useCurrentFrame} from "remotion";
 
 export const AnimatedBackground: React.FC = () => {
